7acc234404bba234346bda63ae115631c1e7c602
bboudou <berengere94@gmail.com>
2025-01-08 15:49:37 +0100

    #8 create sample ressource

---
+++ src/main/java/fr/efrei/rag/web/rest/SampleResource.java
@@ -0,0 +1,16 @@
+package fr.efrei.rag.web.rest;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class SampleResource {
+
+    @GetMapping("/sample/{value}")
+    public String hello(@PathVariable(value = "value", required = false) final String value){
+        return "Hello " + value + "!";
+    }
+}
