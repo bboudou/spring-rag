7f16d43d6e06ee192d4e8af6fe0f7fcf79cb4377
bboudou <berengere94@gmail.com>
2025-01-29 09:57:10 +0100

    #15 fix

--- src/main/java/fr/efrei/rag/web/rest/SampleResource.java
+++ src/main/java/fr/efrei/rag/web/rest/SampleResource.java
@@ -1,6 +1,7 @@
 package fr.efrei.rag.web.rest;
 
 
+import fr.efrei.rag.web.dto.Sample;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class SampleResource {
 
-    @GetMapping("/sample/{value}")
+    @GetMapping("/samples/{value}")
     public String hello(@PathVariable(value = "value", required = false) final String value){
         return "Hello " + value + "!";
+    }
+    @GetMapping("/samples/dto/{value}")
+    public Sample helloDto(@PathVariable(value = "value", required = false) final String value) {
+        return new Sample(value);
     }
 }
